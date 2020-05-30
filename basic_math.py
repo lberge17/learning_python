54413d6eb9edbf3e6fce0d6cb4d232be0ebd9d5b
Laura Berge <lauraeberge@gmail.com>
2020-05-30 15:09:22 -0500

    adds a few more basic math functions like multiplication, division, and remainder

--- basic_math.py
+++ basic_math.py
@@ -6,8 +6,25 @@ def sum(a, b):
 def difference(a, b):
     return a - b
 
+# write a function that takes in two numbers as arguments and returns their product
+def product(a, b):
+    return a * b
 
+# write a function that takes in two numbers as arguments and returns their quotient (float if there is a remainder)
+def quotient(a, b):
+    if a % b > 0:
+        return a * 1.0 / b
+    else:
+        return a / b
+
+# write a function that takes in two numbers as arguments and returns their remainder
+def remainder(a, b):
+    return a % b
 
 
 print(sum(3, 2))
 print(difference(5, 4))
+print(product(7, 3))
+print(quotient(15, 3))
+print(quotient(16, 3))
+print(remainder(19, 5))
