bc58b7dfb0bcb1f62fc05e95ffcda177adbeea4e
Laura Berge <lauraeberge@gmail.com>
2020-05-30 15:14:00 -0500

    fixes bug in quotient function so that it now takes into account where given a float or integer and also negative numbers

--- basic_math.py
+++ basic_math.py
@@ -12,10 +12,10 @@ def product(a, b):
 
 # write a function that takes in two numbers as arguments and returns their quotient (float if there is a remainder)
 def quotient(a, b):
-    if a % b > 0:
-        return a * 1.0 / b
+    if a % b == 0:
+        return int(a / b)
     else:
-        return a / b
+        return a * 1.0 / b
 
 # write a function that takes in two numbers as arguments and returns their remainder
 def remainder(a, b):
@@ -25,6 +25,6 @@ def remainder(a, b):
 print(sum(3, 2))
 print(difference(5, 4))
 print(product(7, 3))
-print(quotient(15, 3))
-print(quotient(16, 3))
+print(quotient(15.0, -3))
+print(quotient(16, -3))
 print(remainder(19, 5))
