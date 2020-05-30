35a55170722a39709a39afc05c8d4c79c6f869d7
Laura Berge <lauraeberge@gmail.com>
2020-05-30 14:54:28 -0500

    adds a couple beginning math challenges and their solutions

---
+++ basic_math.py
@@ -0,0 +1,13 @@
+# write a function that takes in two numbers as arguments and returns their sum
+def sum(a, b):
+    return a + b
+
+# write a function that takes in two numbers as arguments and returns their difference
+def difference(a, b):
+    return a - b
+
+
+
+
+print(sum(3, 2))
+print(difference(5, 4))
