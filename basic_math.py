d0a780e8ccf25d350bc72f6c0dcc2c3186930cb2
Laura Berge <lauraeberge@gmail.com>
2020-06-01 09:43:21 -0500

    adds calculator function that takes in operator as well as two numbers, renames add function since sum is keyword

--- basic_math.py
+++ basic_math.py
@@ -1,5 +1,5 @@
 # write a function that takes in two numbers as arguments and returns their sum
-def sum(a, b):
+def add(a, b):
     return a + b
 
 # write a function that takes in two numbers as arguments and returns their difference
@@ -21,10 +21,25 @@ def quotient(a, b):
 def remainder(a, b):
     return a % b
 
+# write a function that takes two numbers and an operator and return the result
+# I'm sure this is way more lengthy than need be a could be refactored
+def calculator(a, b, op):
+    if op.lower() == 'add':
+        return add(a, b)
+    elif op.lower() == 'subtract':
+        return difference(a, b)
+    elif op.lower() == 'multiply':
+        return product(a, b)
+    elif op.lower() == 'divide':
+        return quotient(a, b)
+    elif op.lower() == 'remainder':
+        return remainder(a, b)
 
-print(sum(3, 2))
+
+print(add(3, 2))
 print(difference(5, 4))
 print(product(7, 3))
 print(quotient(15.0, -3))
 print(quotient(16, -3))
 print(remainder(19, 5))
+print(calculator(3,5,'multiply'))
