771e0f48d1defb2911cfcecbb50545bf6f788db6
Laura Berge <lauraeberge@gmail.com>
2020-06-12 11:15:59 -0500

    adds notes about printing multiple items in one line using a comma

--- strings.py
+++ strings.py
@@ -2,4 +2,9 @@ my_string = "Hello world! I'm learning Python."
 
 print "Hello"
 print my_string
+
+# using square brackets to access a range of characters
 print my_string[26:30] # prints "Pyth"
+
+# using commas to print two items
+print "My message is:", my_string[13:33] # prints "My message is: I'm learning Python."
