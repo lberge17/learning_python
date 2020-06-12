93bff483efb0d2bb5b5ff7bd037bb3d3d3dd7429
Laura Berge <lauraeberge@gmail.com>
2020-06-12 11:24:49 -0500

    switches syntax to be compatible with python 3 so I could include string interpolation

--- strings.py
+++ strings.py
@@ -1,10 +1,13 @@
 my_string = "Hello world! I'm learning Python."
 
-print "Hello"
-print my_string
+print("Hello")
+print(my_string)
 
 # using square brackets to access a range of characters
-print my_string[26:30] # prints "Pyth"
+print(my_string[26:30]) # prints "Pyth"
 
 # using commas to print two items
-print "My message is:", my_string[13:33] # prints "My message is: I'm learning Python."
+print("My message is:", my_string[13:33]) # prints "My message is: I'm learning Python."
+
+# using string interpolation
+print(f'My message is: {my_string}')
